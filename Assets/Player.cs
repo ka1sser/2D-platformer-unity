40a3441c495e3e4d5e938fd38e432988ac628b6b
Kaisser Sereno <serenokaisser.dev@gmail.com>
2025-01-11 22:16:33 +0800

    Initial x-axis movement for the player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,11 +4,23 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    [SerializeField]
-    private float speed;
+    [SerializeField] private float moveSpeed;
+    private float xInput;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
     }
 }
