56996ad6ba527cc4d97f71e6bed7d63bc214d0ab
Kaisser Sereno <serenokaisser.dev@gmail.com>
2025-01-13 23:17:54 +0800

    Adding the jump feature and cleaning the code. Used raycast to detect the distance between ground and player.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
 
+    [Header("Movement Configuration")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    
     private float xInput;
 
     private void Awake()
@@ -16,11 +26,41 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
-        
-    
+        HandleCollision();
+        HandleInput();
         HandleMovement();
         HandleAnimations();
+    }
+
+    private void HandleCollision()
+    {
+        /*
+        This function detects the ground via Raycast. To check the value for the ground distance, we
+        used the OnDrawGizmos() to draw a line from the origin to the distance in y.
+        */
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 
     private void HandleMovement()
