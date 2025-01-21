de0691bee1c5de5884abe5f8ded36279be9ec50c
Kaisser Sereno <serenokaisser.dev@gmail.com>
2025-01-21 21:12:52 +0800

    Trigger scripts and some cleaning on Unity interface

---
+++ Assets/Scripts/DamageTrigger.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+
+        if (player != null)
+        {
+            player.KnockBack();
+        }
+    }
+}
