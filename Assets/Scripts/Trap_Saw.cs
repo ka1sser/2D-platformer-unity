0082435aa068264bb3d33ff4d7f4c26c08c3357d
ka1sser <serenokaisser.dev@gmail.com>
2025-02-03 23:15:32 +0800

    Done preparing the trap_saw and created prefab

--- Assets/Scripts/Trap_Saw.cs
+++ Assets/Scripts/Trap_Saw.cs
@@ -9,8 +9,10 @@ public class Trap_Saw : MonoBehaviour
     [SerializeField] private float moveSpeed = 3;
     [SerializeField] private Transform[] wayPoints;
     [SerializeField] private float cooldown = 1;
+    private Vector3[] wayPointPosition;
 
     public int wayPointIndex = 1;
+    public int moveDirection = 1;
     private bool canMove = true;
 
     private void Awake()
@@ -19,9 +21,20 @@ public class Trap_Saw : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
     }
 
-    private void Start() 
+    private void Start()
     {
-        transform.position = wayPoints[0].position;
+        UpdateWaypointInfo();
+        transform.position = wayPointPosition[0];
+        
+    }
+
+    private void UpdateWaypointInfo()
+    {
+        wayPointPosition = new Vector3[wayPoints.Length];
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            wayPointPosition[i] = wayPoints[i].position;
+        }
     }
 
     private void Update() 
@@ -31,18 +44,19 @@ public class Trap_Saw : MonoBehaviour
         if (canMove == false)
             return;
 
-        transform.position = Vector2.MoveTowards(transform.position, wayPoints[wayPointIndex].position, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, wayPointPosition[wayPointIndex], moveSpeed * Time.deltaTime);
 
-        if (Vector2.Distance(transform.position, wayPoints[wayPointIndex].position) < .1f )
+        if (Vector2.Distance(transform.position, wayPointPosition[wayPointIndex]) < .1f )
         {
-            wayPointIndex++;
-            sr.flipX = !sr.flipX;
             
-            if (wayPointIndex >= wayPoints.Length)
+            if (wayPointIndex == wayPointPosition.Length - 1 || wayPointIndex == 0)
             {
-                wayPointIndex = 0;
+                moveDirection = moveDirection * -1;
                 StartCoroutine(StopMovement(cooldown));
             }
+
+            wayPointIndex = wayPointIndex + moveDirection;
+
         }
 
     }
@@ -54,5 +68,6 @@ public class Trap_Saw : MonoBehaviour
         yield return new WaitForSeconds(delay);
 
         canMove = true;
+        sr.flipX = !sr.flipX;
     }
 }
